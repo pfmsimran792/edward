add724b08a987f22d0f5ffd5d0746eebcfb4fe78
Imran Ahamad <pfms.imran792@gmail.com>
2025-08-31 04:16:16 +0000

    docker change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=9000)
